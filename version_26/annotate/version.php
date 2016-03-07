fa40dfb9050d592b6852e85e0b0eb9f73d17710e
Fokion <fokion@textensor.com>
2016-03-07 11:22:27 +0000

    Fix for 1.3 we require only the resource id to get the course details from the context

--- version_26/annotate/version.php
+++ version_26/annotate/version.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 //see http://docs.moodle.org/dev/Releases
 $plugin->version=2013112700;
 $plugin->requires = 2012062500;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '1.3';
+$plugin->release = '1.3.1';
 $plugin->component = 'block_annotate';
